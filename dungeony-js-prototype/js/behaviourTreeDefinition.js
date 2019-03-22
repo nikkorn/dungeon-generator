7a53d3b5f6057040e540fc26901ce849ed6d547d
nikkorn <nikhoward1234@hotmail.co.uk>
2019-03-22 22:57:46 +0000

    Tidying test BT definition

--- dungeony-js-prototype/js/behaviourTreeDefinition.js
+++ dungeony-js-prototype/js/behaviourTreeDefinition.js
@@ -2,105 +2,103 @@
 
 // TERMINOLOGY
 
-// CONTEXT
-// A context represents state for a node and any children node in the tree.
+// BLACKNOARD
+// An external object that is passed throughout the tree to hold blackboard and functions.
+// The blackboard is never destroyed.
 
-// Moving to a new node in the tree will create a brand new context object for the node.
-// This context will be passed in to every 'test()', 'onEntry()', 'onUpdate()' and 'onExit()' call for that node.
-// The context is thrown away when a node stops being visited (is no longer active).
-// Each context object will have a 'getParentContext()' function on it which will return the context of the parent node.
+// TODO Add decorator nodes. A decorator node has a single child an can modify its behaviour (e.g. make it loop, invert its return type).
 
-// STATE
-// An external object that is passed throughout the tree to be used much in the same way as context objects.
-// The state is never destroyed.
+// Sequence and selector nodes have onEntry and onExit functions, while actions also have a onUpdate function from which we return either:
+//    SUCCESS: The node was completed succesfully. If the node is one in a sequence then we should move on to the next, or if it is the last then the sequence node returns SUCCESS.
+//    FAIL   : The node failed to complete. If the node is one in a sequence then do no process any following nodes and then the sequence node returns FAIL.
+//    RESUME : The node is still active.
+
+// TODO Add ability to register a node to be reused.
 
 const behaviourTreeDefinition = [
     {
-        type: "branch",
+        type: "selector",
         nodes: [
             {
-                type: "node",
+                type: "action",
                 name: "patrolling",
-                test: (context, state) => !state.canSeePlayer(),
-                onEntry: (context, state) => console.log("Starting Patrol!"),
-                onUpdate: (context, state) => {},
-                onExit: (context, state) => console.log("Stopping Patrol!")
+                test: (blackboard) => !blackboard.canSeePlayer(),
+                onEntry: (blackboard) => console.log("Starting Patrol!"),
+                onUpdate: (blackboard) => {},
+                onExit: (blackboard) => console.log("Stopping Patrol!")
             },
             {
-                type: "cycle",
+                type: "sequence",
                 name: "spotted_player",
                 nodes: [
                     {
-                        type: "node",
+                        type: "action",
                         name: "act_suprised",
-                        test: (context, state) => context.hasJustSpottedPlayer(),
-                        onEntry: (context, state) => console.log("Starting acting surprised after seeing player!"),
-                        onUpdate: (context, state) => {},
-                        onExit: (context, state) => console.log("Stopping acting surprised after seeing player!")
+                        test: (blackboard) => blackboard.hasJustSpottedPlayer(),
+                        onEntry: (blackboard) => console.log("Starting acting surprised after seeing player!"),
+                        onUpdate: (blackboard) => {},
+                        onExit: (blackboard) => console.log("Stopping acting surprised after seeing player!")
                     },
                     {
-                        type: "branch",
+                        type: "selector",
                         name: "following_player",
                         nodes: [
                             {
-                                type: "node",
+                                type: "action",
                                 name: "pursue_player",
-                                test: (context, state) => !context.isPlayerInAttackDistance(),
-                                onEntry: (context, state) => console.log("Walking towards the player!"),
-                                onUpdate: (context, state) => {},
-                                onExit: (context, state) => console.log("Stopped walking towards the player!")
+                                test: (blackboard) => !blackboard.isPlayerInAttackDistance(),
+                                onEntry: (blackboard) => console.log("Walking towards the player!"),
+                                onUpdate: (blackboard) => {},
+                                onExit: (blackboard) => console.log("Stopped walking towards the player!")
                             },
                             {
-                                type: "cycle",
+                                type: "sequence",
                                 name: "attacking_player_cycle",
                                 nodes: [
                                     {
-                                        type: "node",
+                                        type: "action",
                                         name: "attack_player",
-                                        test: (context, state) => !state.isPlayerInAttackDistance(),
-                                        onEntry: (context, state) => console.log("Walking towards the player!"),
-                                        onUpdate: (context, state) => {
+                                        test: (blackboard) => !blackboard.isPlayerInAttackDistance(),
+                                        onEntry: (blackboard) => console.log("Walking towards the player!"),
+                                        onUpdate: (blackboard) => {
                                             // Hit the player super hard!
-                                            state.attackPlayer();
+                                            blackboard.attackPlayer();
 
-                                            // TODO Set cool down on node state.
+                                            // TODO Set cool down on node blackboard.
                                         },
-                                        onExit: (context, state) => console.log("Stopped walking towards the player!")
+                                        onExit: (blackboard) => console.log("Stopped walking towards the player!")
                                     },
                                     {
-                                        type: "node",
+                                        type: "action",
                                         name: "attack_cool_down_wait",
-                                        test: (context, state) => !state.isPlayerInAttackDistance(),
-                                        onEntry: (context, state) => console.log("Taking a break from attacking!"),
-                                        onUpdate: (context, state) => {},
-                                        onExit: (context, state) => console.log("Stopped walking towards the player!")
+                                        test: (blackboard) => !blackboard.isPlayerInAttackDistance(),
+                                        onEntry: (blackboard) => console.log("Taking a break from attacking!"),
+                                        onUpdate: (blackboard) => {},
+                                        onExit: (blackboard) => console.log("Stopped walking towards the player!")
                                     }
                                 ],
-                                test: (context, state) => !context.isPlayerInAttackDistance(),
-                                onEntry: (context, state) => console.log("Walking towards the player!"),
-                                onUpdate: (context, state) => {},
-                                onExit: (context, state) => console.log("Stopped walking towards the player!")
+                                test: (blackboard) => !blackboard.isPlayerInAttackDistance(),
+                                onEntry: (blackboard) => console.log("Walking towards the player!"),
+                                onExit: (blackboard) => console.log("Stopped walking towards the player!")
                             }
 
                         ],
-                        test: (context, state) => context.canFollowPlayer(),
-                        onEntry: (context, state) => console.log("Starting to follow the player!"),
-                        onUpdate: (context, state) => {},
-                        onExit: (context, state) => {}
+                        test: (blackboard) => blackboard.canFollowPlayer(),
+                        onEntry: (blackboard) => console.log("Starting to follow the player!"),
+                        onExit: (blackboard) => {}
                     },
                     {
-                        type: "node",
+                        type: "action",
                         name: "lost_player",
-                        test: (context, state) => context.hasJustLostPlayer(),
-                        onEntry: (context, state) => console.log("Just lost the player!"),
-                        onUpdate: (context, state) => {},
-                        onExit: (context, state) => {}
+                        test: (blackboard) => blackboard.hasJustLostPlayer(),
+                        onEntry: (blackboard) => console.log("Just lost the player!"),
+                        onUpdate: (blackboard) => {},
+                        onExit: (blackboard) => {}
                     }
                 ],
-                test: (context, state) => state.canSeePlayer(),
-                onEntry: (context, state) => console.log("Starting the spotted player cycle!"),
-                onUpdate: (context, state) => {},
-                onExit: (context, state) => console.log("Stopping the spotted player cycle!")
+                test: (blackboard) => blackboard.canSeePlayer(),
+                onEntry: (blackboard) => console.log("Starting the spotted player sequence!"),
+                onExit: (blackboard) => console.log("Stopping the spotted player sequence!")
             }
         ]
     }
